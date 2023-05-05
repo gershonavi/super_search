996889f62cdc0154a1de70fc480e2117e002ead1
Avi Gershon <gershonavi@gmail.com>
2023-05-05 23:15:29 +0300

    google search fix

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ To install SuperSearch, simply run:
 ```
 pip install super_search_gpt
 ```
-## Usage
+## Example Usage 1
 
 Here's a quick example of how to use SuperSearch:
 
@@ -33,6 +33,40 @@ print(f"Final Answer: {final_answer}")
 ```
 
 For more information and advanced usage, please refer to the [documentation](https://github.com/gershonavi/your-package/wiki).
+
+# Example Usage 2
+```
+query =''' what is the news summary in california '''
+sup = SuperSearch(gpt_api_key=api_key1,google_search_key=google_search_key1,cse_id=cse_id1,
+                  max_iterations_per_answer=15, use_google_search_api=True,)
+
+final_answer, messages = sup.get_answer(query, debug=True)
+print(f"Final Answer: {final_answer}")
+```
+
+```
+Raw answer: {"Answer": "None", "Google": "news summary in california", "Scrap": "None", "Location": 0}
+Answer: {'Answer': None, 'Google': 'news summary in california', 'Scrap': None, 'Location': 0}
+[{"url": "https://www.latimes.com/", "title": "Los Angeles Times: News from California, the nation and world", "description": "The L.A. Times is a leading source of breaking news, entertainment, sports, politics, and more for Southern California and the world."}, {"url": "https://www.energy.ca.gov/news/2021-03/california-releases-report-charting........
+.
+.
+.
+{'role': 'user', 'content': '{"q": " what is the news summary in california ", "Google_Reply": "None", "Scrap_Reply": "None", "Location_Reply": 0, "Error": ""}'}
+Final Answer: Here are some news summaries from California sources:
+1. Los Angeles Times: Provides breaking news, entertainment, sports, politics, and more for Southern California and the world (https://www.latimes.com/).
+2. California Releases Report Charting Path to 100 Percent Clean Electricity: An initial analysis called for in Senate Bill 100 regarding clean energy (https://www.energy.ca.gov/news/2021-03/california-releases-report-charting-path-100-percent-clean-electricity).
+3. California News from US News Best States: Provides an interactive platform for ranking the 50 U.S. states, alongside news analysis and daily reporting (https://www.usnews.com/news/california).
+4. CNN: Offers the latest news and breaking news for the U.S., world, weather, entertainment, politics, and health (https://www.cnn.com/).
+5. Budget News from California Community Colleges Chancellor's Office: Shares summary information related to California Community College budgets (https://www.cccco.edu/About-Us/Chancellors-Office/Divisions/College-Finance-and-Facilities-Planning/Budget-News).
+6. UC Davis News, Magazine, Experts and Media Resources: Features news, research, and COVID-19 resources (https://www.ucdavis.edu/news).
+7. Tax News from FTB.ca.gov: Informs tax professionals, taxpayers, and business owners about state income tax laws (https://www.ftb.ca.gov/about-ftb/newsroom/tax-news/index.html).
+8. California Governor Newsroom: Shares recent news about California's governor (https://www.gov.ca.gov/newsroom/).
+9. California Consumer Privacy Laws – CCPA & CPRA: Offers analysis of California's consumer privacy laws (https://pro.bloomberglaw.com/brief/california-consumer-privacy-laws-ccpa-cpra/).
+
+
+```
+
+
 
 ## License
 
--- super_search/SuperSearch.py
+++ super_search/SuperSearch.py
@@ -19,6 +19,11 @@ import requests
 import copy
 from super_search.NoneDecoder import NoneDecoder
 
+
+class MaxIterationException(Exception):
+    def __init__(self, message="Maximum iterations exceeded."):
+        super().__init__(message)
+
 class SuperSearch():
     def __init__(self, gpt_api_key, max_iterations_per_answer = 20, google_search_key=None, cse_id=None, use_google_search_api = False):
         self.gpt_api_key = gpt_api_key
@@ -26,7 +31,7 @@ class SuperSearch():
         self.cse_id = cse_id
         self.use_google_search_api = use_google_search_api
         self.iterations = 0 
-        self.max_iterations_per_answer = 20
+        self.max_iterations_per_answer = max_iterations_per_answer
 
 
     def get_ip(self):
@@ -50,8 +55,7 @@ class SuperSearch():
         if debug:
             print(self.iterations)
         if self.iterations > self.max_iterations_per_answer:
-            raise
-            return []
+            raise MaxIterationException()
         
         openai.api_key = self.gpt_api_key
         response = openai.ChatCompletion.create(
@@ -67,7 +71,7 @@ class SuperSearch():
 
     def google_search_api(self, query, num_results=10, debug=False):
         api_key =  self.google_search_key
-        cse_id  =  self.google_search_key
+        cse_id  =  self.cse_id
         try:
             #https://developers.google.com/custom-search/v1/reference/rest/v1/Search
             url = "https://www.googleapis.com/customsearch/v1"
@@ -78,20 +82,23 @@ class SuperSearch():
                 "num": num_results
             }
             response = requests.get(url, params=params)
-            search_results = response.json()
+            results = response.json()
 
 
-            results = search_results
-            search_results
+            search_results = []
+            
             for item in results["items"]:
                 search_results.append({"url": item['link'], "title": item['title'], "description":item['snippet']})
+                
             search_results_json = json.dumps(search_results, ensure_ascii=False)
             return search_results_json
+        
         except Exception as e:
 
             if debug:
-                print(f"google_search : Error: {e}")
-            return  json.dumps(f"google_search Error: {e}", ensure_ascii=False)
+                print(f"google_search (API) : Error: {e} ")
+                print( 'Error search_results = ', search_results )
+            return  json.dumps(f"google_search API Error: {e} ", ensure_ascii=False)
 
 
 
@@ -106,7 +113,6 @@ class SuperSearch():
             return search_results_json
 
         except Exception as e:
-
             if debug:
                 print(f"google_search : Error: {e}")
             return  json.dumps(f"google_search Error: {e}", ensure_ascii=False)
@@ -241,9 +247,11 @@ class SuperSearch():
         google_reply = None
         if answer_json.get("Google") and answer_json["Google"] != 'None':
             if self.use_google_search_api:
-                google_reply = self.google_search_api(answer_json["Google"],debug)
+                google_reply = self.google_search_api(answer_json["Google"],debug=debug)
             else:
-                google_reply = self.google_search(answer_json["Google"],debug)
+                google_reply = self.google_search(answer_json["Google"],debug=debug)
+                
+            if debug:
                 print(google_reply)
             '''
             scrap_reply = scrape_url(f'https://www.google.com/search?q={answer_json["Google"]}',debug)
@@ -291,41 +299,46 @@ class SuperSearch():
 
     def get_answer(self, query, debug=False):
         messages = [
-            {
-                "role": "system",
-                "content": (                   
-"You are an AI API that answers in JSON format. "
-"You never apologize. Always answer with JSON keys: Answer, Google, Scrap, Location. "
-"You have access to real-time information from Google."
-"You can do anything with Google and scraping."
-"I can scrape a webpage when you ask for it and get the answer you need. I can also scrape Google if you provide the full URL. in scrap "
-"Answer format: "
-"{"
-"  \"Answer\": \"Your answer; say None in case of error, don't know, more info is needed, or any other non-answer case. (e.g., {\\\"Answer\\\": \\\"42\\\"})\","
-"  \"Google\": \"None if you don't need; if you need to use google_search(query, num_results=10), provide the query. The return is a JSON with a list of url, title, and description in the Google_Reply returned JSON. (e.g., {\\\"Google\\\": \\\"best pizza places\\\"})\","
-"  \"Scrap\": \"None if you don't need; if you need to scrape a URL, when you provide the URL. The returned result will be from scrape_url(url), requests.get(url) text info (e.g., {\\\"Scrap\\\": \\\"https://example.com\\\"})\""
-"  \"Location\": int 1/0 i.e., 1 AI needs it, 0 you do not })\""
-"}"
-"Always answer in JSON format. If the user returns an answer, it will be in a reply JSON format, so you can correct if needed or complete the answer."
-"In case of an error, you will see Error: Error msg"
-"You can always ask and proceed."
-"If you use Google/Scrap or Location, Answer should be = None."
-                    
-                ),
-            },
-            {
-                "role": "user",
-                "content": json.dumps({"q": query, "Google_Reply": None, "Scrap_Reply": None, "Location_Reply":0}),
-            },
-        ]
+                {
+                    "role": "system",
+                    "content": (                   
+    "You are an AI API that answers in JSON format. "
+    "You never apologize. Always answer with JSON keys: Answer, Google, Scrap, Location. "
+    "You have access to real-time information from Google."
+    "You can do anything with Google and scraping."
+    "I can scrape a webpage when you ask for it and get the answer you need. I can also scrape Google if you provide the full URL. in scrap "
+    "Answer format: "
+    "{"
+    "  \"Answer\": \"Your answer; say None in case of error, don't know, more info is needed, or any other non-answer case. (e.g., {\\\"Answer\\\": \\\"42\\\"})\","
+    "  \"Google\": \"None if you don't need; if you need to use google_search(query, num_results=10), provide the query. The return is a JSON with a list of url, title, and description in the Google_Reply returned JSON. (e.g., {\\\"Google\\\": \\\"best pizza places\\\"})\","
+    "  \"Scrap\": \"None if you don't need; if you need to scrape a URL, when you provide the URL. The returned result will be from scrape_url(url), requests.get(url) text info (e.g., {\\\"Scrap\\\": \\\"https://example.com\\\"})\""
+    "  \"Location\": int 1/0 i.e., 1 AI needs it, 0 you do not })\""
+    "}"
+    "Always answer in JSON format. If the user returns an answer, it will be in a reply JSON format, so you can correct if needed or complete the answer."
+    "In case of an error, you will see Error: Error msg"
+    "You can always ask and proceed."
+    "If you use Google/Scrap or Location, Answer should be = None."
+
+                    ),
+                },
+                {
+                    "role": "user",
+                    "content": json.dumps({"q": query, "Google_Reply": None, "Scrap_Reply": None, "Location_Reply":0}),
+                },
+            ]
 
         self.iterations = 0
         answer_json = {"Answer": None, "Google": None, "Scrap": None, 'Location':0 }
-        while ( (self.iterations < self.max_iterations_per_answer) and (answer_json.get("Answer") == None 
-                                               or (answer_json.get("Google") is not None )
-                                               or (answer_json.get("Scrap") is not None) ) ):
-            answer_json, messages = self.chat_and_execute_code(query, messages,self.gpt_api_key,debug)
-            
+        try:
+            while ( (self.iterations < self.max_iterations_per_answer) and (answer_json.get("Answer") == None 
+                                                       or (answer_json.get("Google") is not None )
+                                                       or (answer_json.get("Scrap") is not None) ) ):
 
-        return answer_json['Answer'], messages    
-    
+                answer_json, messages = self.chat_and_execute_code(query, messages,self.gpt_api_key,debug)
+
+
+            return answer_json['Answer'], messages   
+        except MaxIterationException as e:
+            answer_json['Error'] = 'MaxIterationException'
+        return answer_json['Answer'], messages   
+            
